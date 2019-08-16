3230fa381857fdc26a4451fc5e314663bb414205
thongchaic <thongchai.c@srru.ac.th>
2019-08-16 14:29:37 +0700

    v3

--- Robot162/iot_robot_v3.py
+++ Robot162/iot_robot_v3.py
@@ -12,7 +12,10 @@ CFG_BSSID_PASS='SrruIoT@2019'
 
 FRONT_LED = machine.Pin(2, machine.Pin.OUT)
 P1 = machine.Pin(16, machine.Pin.OUT)
-P2 = machine.Pin(4, machine.Pin.OUT)
+P2 = machine.Pin(5, machine.Pin.OUT)
+P3 = machine.Pin(4, machine.Pin.OUT)
+P4 = machine.Pin(0, machine.Pin.OUT)
+
 FRONT = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=1000000)
 
 def __init__():
