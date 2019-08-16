dacbd431ebbad848e2b0a25580d2b2246374d9fe
thongchaic <thongchai.c@srru.ac.th>
2019-08-16 15:18:53 +0700

    wheel control

--- Robot162/iot_robot_v3.py
+++ Robot162/iot_robot_v3.py
@@ -19,11 +19,11 @@ P4 = machine.Pin(0, machine.Pin.OUT)
 FRONT = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=1000000)
 
 def __init__():
-	print("INIT")
-	print('Frequency ', machine.freq())
-	FRONT_LED.value(1)
-	ap = network.WLAN(network.AP_IF)
-	ap.active(False)
+    print("INIT")
+    print('Frequency ', machine.freq())
+    FRONT_LED.value(1)
+    ap = network.WLAN(network.AP_IF)
+    ap.active(False)
 
 def do_connect():
 
@@ -46,36 +46,41 @@ def do_connect():
 
 def stop():
     print("stop")
-	FRONT_LED.value(0)
+    FRONT_LED.value(0)
     P1.on()
     P2.on()
+    P3.on()
+    P4.on()
 
 def forward():
-	print("forward....")
-	P1.on()
-	P2.off()
+    print("forward....")
+    P1.off()
+    P2.on()
+    P3.off()
+    P4.on()
 
 def backward():
-	print("backward....")
-	#P1.off()
-	#P2.off()
+    print("backward....")
+    #P1.off()
+    #P2.off()
 
 def start_my_car():
+    while True:
+ 	front_cm = FRONT.distance_cm()
+	print("Front=>", front_cm)
+	if front_cm < 20:
+	    stop()
+	else:
+	    forward()
 
-	while True:
- 		front_cm = FRONT.distance_cm()
-		print("Front=>", front_cm)
-		if front_cm < 80:
-			stop()
-		else:
-			forward()
-
-		time.sleep(0.5)
+	time.sleep(0.5)
 
 
 if __name__ == '__main__':
 
-	__init__()
-	#start_ap(False)
-	#do_connect()
-	start_my_car()
+    __init__()
+    #start_ap(False)
+    #do_connect()
+    start_my_car()
+
+
